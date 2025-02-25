163b7b36b00f49abd0cac918cdb6ef058fdbfd9d
Acxel Morales <39923586+AcxelMorales@users.noreply.github.com>
2025-02-25 11:52:17 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
-  giphyApiKey: 'cGZixQq0eWS5z1DekDTM31z1HdZ4r5ew',
+  giphyApiKey: '',
   giphyUrl: 'https://api.giphy.com/v1/gifs',
 };
